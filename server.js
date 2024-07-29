c4219b11a5e7ae0f61f0a523a41773be67ba1791
Pavan_Kumar <pavankumar.99gpk@gmail.com>
2024-07-29 21:50:30 +0530

    added all the file that is required!!! and first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+let port = process.env.PORT || 5000
+const app = express();
+
+app.listen(port,()=>{
+    console.log(`Server Started in port 8080!!!`);
+});
+
+app.use();
+
+app.get('about',(req,response)=>{
+    response.send(`<h1>Hey Here Iam </h1>`);
+})
+
+app.get('home',(req,response)=>{
+    response.send(`<h1>Hey Here Iam </h1>`);
+})
+
+app.get('product:Id',(req,response)=>{
+    response.send(`<h1>Hey Here Iam </h1>`);
+})
